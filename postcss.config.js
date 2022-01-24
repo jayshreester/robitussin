4fe46a7cc32e8d13e925d2685f465475e2185b0c
Abdul Wahid Mohd Aslam Dunga <Abdul Wahid Mohd Aslam Dunga@accionlabs.in>
2022-01-25 00:33:16 +0530

    added banner

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-
-    plugins: (loader) => [
-      require('autoprefixer')({
-        browsers: [
-          'last 2 versions',
-          '> 1%',
-        ], // browser options can be configured from https://github.com/browserslist/browserslist
-      }),
-    ]
--- src/js/components/no-search-result-box.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-  $(document).ready(function() {
-    if ($('.searchResults-results').length) {
-      $('.no-search-result-box').hide();
-    }
-  });
-})(Cog.jQuery());
