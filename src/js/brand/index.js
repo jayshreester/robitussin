75a37d4af3f897b9cd1ced5e1d06c283bef6b443
Abdul Wahid Mohd Aslam Dunga <Abdul Wahid Mohd Aslam Dunga@accionlabs.in>
2022-01-27 12:52:31 +0530

    Update JS

--- src/js/brand/index.js
+++ src/js/brand/index.js
@@ -1,68 +1,64 @@
 (function($) {
-  var mobileTarget = $('.tabs-nav-list');
-  var desktopTarget = $('.tabs > .component-content');
-  var productTabs = $('.new-product-tabs');
+    var mobileTarget = $('.tabs-nav-list');
+    var desktopTarget = $('.tabs > .component-content');
+    var productTabs = $('.new-product-tabs');
 
-  function moveTabs(target) {
-    $('.tabs-content').each(function(i, v) {
-      target.append($(this));
-    });
-  }
-
-  function tabsReWrap() {
-    if ($(window).width() < 992) {
-      moveTabs(mobileTarget);
-      productTabs.addClass('mobile-tabs');
-    } else {
-      moveTabs(desktopTarget);
-      productTabs.removeClass('mobile-tabs');
+    function moveTabs(target) {
+        $('.tabs-content').each(function(i, v) {
+            target.append($(this));
+        });
     }
-  }
-  if ($('.new-product-tabs').length) {
-    $(document).ready(function() {
-      tabsReWrap();
+
+    function tabsReWrap() {
+        if ($(window).width() < 992) {
+            moveTabs(mobileTarget);
+            productTabs.addClass('mobile-tabs');
+        } else {
+            moveTabs(desktopTarget);
+            productTabs.removeClass('mobile-tabs');
+        }
+    }
+    if ($('.new-product-tabs').length) {
+        $(document).ready(function() {
+            tabsReWrap();
+        });
+
+        $(window).resize(function() {
+            tabsReWrap();
+        });
+    }
+    //accordion starts
+    function customacc() {
+        $('.accordion-head').click(function() {
+            $(this)
+                .parent()
+                .toggleClass('is-active');
+        });
+    }
+    if ($('.accordion').length) {
+        $(document).ready(function() {
+            customacc();
+        });
+    }
+    //accordion ends
+    $('.navigation-item-decoration').each(function() {
+        $(this).removeAttr('tabindex');
     });
 
-    $(window).resize(function() {
-      tabsReWrap();
+    $('.country-selected').each(function() {
+        $(this).removeClass('external');
     });
-  }
-  function filtercon() {
-    $('.filteraccord').click(function() {
-      $('.filtercontent').slideToggle(), $(this).toggleClass('active');
-    });
-  }
 
-  if ($('.filteraccord').length) {
-    $(document).ready(function() {
-      filtercon();
-    });
-  }
-
-  //accordion starts
-  function customacc() {
-    $('.accordion-head').click(function() {
-      $(this)
-        .parent()
-        .toggleClass('is-active');
-    });
-  }
-  if ($('.accordion').length) {
-    $(document).ready(function() {
-      customacc();
-    });
-  }
-  //accordion ends
-  $('.navigation-item-decoration').each(function() {
-    $(this).removeAttr('tabindex');
-  });
-
-  $('.country-selected').each(function() {
-    $(this).removeClass('external');
-  });
-
-  $('body').on('click touchstart', '.filter-item-js', function(e) {
-    var seltext = $('.selected-item-js').text();
-    $('.filteredtext').text(seltext);
-  });
+    if ($(window).width() >= 768) {
+        $('.object1, .object2, .absolute-img-right').mouseover(function() {
+            $(this)
+                .find('.tooltip-data')
+                .show();
+        });
+        $('.object1, .object2, .absolute-img-right').mouseout(function() {
+            $(this)
+                .find('.tooltip-data')
+                .hide();
+        });
+    }
 })(Cog.jQuery());
